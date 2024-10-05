6692b9b05512762fa727621a55bfcb7d3721b218
fazliddin <ilyosov.fa@gmail.com>
2024-10-05 15:59:07 +0500

    views.py is optimized

--- backend/report/views.py
+++ backend/report/views.py
@@ -1,26 +1,26 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404
 from .models import Report
 from .serializers import ReportSerializer
 
 
 @api_view(['GET'])
 def list_reports(request):
-	reports = Report.objects.all()
+	if request.user.is_staff:
+		reports = get_list_or_404(Report)
 
-	if reports and request.user.is_staff:
 		serializer = ReportSerializer(reports, many=True)
+		
 		return Response(serializer.data, status=status.HTTP_200_OK)
 
-	return Response({"detail": "No reports found or you do not have permission"}, status=status.HTTP_404_NOT_FOUND)
+	return Response({"detail": "You are not a staff"}, status=status.HTTP_403_FORBIDDEN)
 
 
 @api_view(['POST'])
 def create_report(request):
     serializer = ReportSerializer(data=request.data)
-    print(request.user)
 
     if serializer.is_valid():
         serializer.save(user=request.user)
@@ -32,9 +32,9 @@ def create_report(request):
 @api_view(['PUT'])
 def update_report(request, pk):
 	if request.user.is_staff:
-		report = Report.objects.get(id=pk)
+		report = get_object_or_404(Report, id=pk)
 	else:
-		report = Report.objects.get(id=pk, user=request.user)
+		report = get_object_or_404(Report, id=pk, user=request.user)
 		
 	serializer = ReportSerializer(instance=report, data=request.data)
 
@@ -55,43 +55,22 @@ def detail_report(request, pk):
 	serializer = ReportSerializer(report)
 
 	return Response(serializer.date, status=status.HTTP_200_OK)
-
-
-# @api_view(['GET'])
-# def detail_report(request, pk):
-# 	try:
-# 		if request.user.is_staff:
-# 			report = Report.objects.get(id=pk)
-# 		else:
-# 			report = Report.objects.get(id=pk, user=request.user)
-
-# 		serializer = ReportSerializer(report)
-# 		return Response(serializer.data, status=status.HTTP_200_OK)		
-# 	except Report.DoesNotExist:
-		
-# 	return Response({'detail': 'Report not found or you do not have permission'}, status=status.HTTP_404_NOT_FOUND)
 	
 
 @api_view(['GET'])
 def user_reports(request):
-	reports = Report.objects.filter(user=request.user)
+	reports = get_list_or_404(Report, user=request.user)
+	serializer = ReportSerializer(reports, many=True)
 
-	if reports:
-		serializer = ReportSerializer(reports, many=True)
-		return Response(serializer.data, status=status.HTTP_200_OK)
-
-	return Response({"detail": "No reports found"}, status=status.HTTP_404_NOT_FOUND)
+	return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
 def admin_reports(request):
 	if request.user.is_staff:
-		reports = Report.objects.filter(admin=request.user)
-
-	if reports:
+		reports = get_list_or_404(Report, admin=request.user)
 		serializer = ReportSerializer(reports, many=True)
+		
 		return Response(serializer.data, status=status.HTTP_200_OK)
 
-	return Response({"detail": "No reports found"}, status=status.HTTP_404_NOT_FOUND)
-
-	
+	return Response({"detail": "You are not a staff"}, status=status.HTTP_403_FORBIDDEN)
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,24 +1,28 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from django.shortcuts import get_object_or_404, get_list_or_404
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
 @api_view(['GET'])
 def list_profiles(request):
-	profiles = Profile.objects.all()
-
-	if profiles:
+	if request.user.is_staff:
+		profiles = get_list_or_404(Profile)
 		serializer = ProfileSerializer(profiles, many=True)
+			
 		return Response(serializer.data, status=status.HTTP_200_OK)
 
-	return Response({"detail": "No profile found"}, status=status.HTTP_404_NOT_FOUND)
+	return Response({"detail": "You are not a staff"}, status=status.HTTP_403_FORBIDDEN)
 
 
 @api_view(['POST'])
 def create_profile(request):
-	serializer = ProfileSerializer(data=request.data)
+	if request.user.is_staff:
+		serializer = ProfileSerializer(data=request.data)
+	else:
+		return Response({"detail": "You are not a staff"}, status=status.HTTP_403_FORBIDDEN)
 
 	if serializer.is_valid():
 		password = serializer.validated_data.pop("password")
@@ -34,7 +38,11 @@ def create_profile(request):
 
 @api_view(['PUT'])
 def update_profile(request, pk):
-	profile = Profile.objects.get(id=pk)
+	if request.user.is_staff:
+		profile = get_object_or_404(Profile, id=pk)
+	else:
+		profile = get_object_or_404(Profile, id=pk, user=request.user)
+
 	serializer = ProfileSerializer(instance=profile, data=request.data)
 
 	if serializer.is_valid():
@@ -51,21 +59,23 @@ def update_profile(request, pk):
 
 @api_view(['GET'])
 def detail_profile(request, pk):
-	profile = Profile.objects.filter(id=pk).first()
+	if request.user.is_staff:
+		profile = get_object_or_404(Profile, id=pk)
+	else:
+		profile = get_object_or_404(Profile, id=pk, user=request.user)
 
-	if profile:
-		serializer = ProfileSerializer(profile)
-		return Response(serializer.data, status=status.HTTP_200_OK)
-
-	return Response({"detail": "Profile not found"}, status=status.HTTP_404_NOT_FOUND)
+	serializer = ProfileSerializer(profile)
+		
+	return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(['DELETE'])
 def delete_profile(request, pk):
-	profile = Profile.objects.filter(id=pk).first()
+	if request.user.is_staff:
+		profile = get_object_or_404(Profile, id=pk)
+	else:
+		profile = get_object_or_404(Profile, id=pk, user=request.user)
 
-	if profile:
-		profile.delete()
-		return Response({"detail": "Profile has been deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+	profile.delete()
 
-	return Response({"detail": "Profile not found"}, status=status.HTTP_404_NOT_FOUND)
+	return Response({"detail": "Profile has been deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
